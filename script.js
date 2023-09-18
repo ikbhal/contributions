8a1d959c5a39e69d374d52ce87c44ec45a433ccc
Shaik Ikbhal Basha <iqbalforall@gmail.com>
2023-09-18 13:40:43 +0530

    color pallet change

--- script.js
+++ script.js
@@ -5,12 +5,53 @@ function getRandomGreen() {
     return `hsl(${hue}, ${saturation}%, ${lightness}%)`;
 }
 
-const gridContainer = document.querySelector('.grid-container');
-
-for (let i = 0; i < 7; i++) {
-    for (let j = 0; j < 52; j++) {
-        const cell = document.createElement('div');
-        cell.style.backgroundColor = getRandomGreen();
-        gridContainer.appendChild(cell);
+// write function to return any one color from the list of colors
+/*
+#196127 dark green
+#7BC96F green
+#BDE4B7 light green
+#C6E48B yellow green
+#EBEDF0 grey
+*/
+function getContributionsColor() {
+    // lets generate count randomly
+    let count = Math.floor(Math.random() * 10);
+    // return '#EBEDF0';
+    if (count < 2) {
+        return '#EBEDF0';
+    } else if (count <= 5) {
+        return '#C6E48B';
+    } else if (count <= 8) {
+        return '#BDE4B7';
+    } else if (count <= 9) {
+        return '#7BC96F';
+    } else {
+        return '#196127';
     }
+}
+
+
+function createGridContainer() {
+    const gridContainer = document.createElement('div');
+    gridContainer.classList.add('grid-container');
+    // const gridContainer = document.querySelector('.grid-container');
+
+    for (let i = 0; i < 7; i++) {
+        for (let j = 0; j < 52; j++) {
+            const cell = document.createElement('div');
+            // cell.style.backgroundColor = getRandomGreen();
+            cell.style.backgroundColor = getContributionsColor();
+            gridContainer.appendChild(cell);
+        }
+    }
+    return gridContainer;
+}
+
+// Create the parent container
+const rowContainer = document.querySelector('.row-container');
+
+// Create and append 7 grid containers to the parent container
+for (let i = 0; i < 7; i++) {
+    const gridContainer = createGridContainer();
+    rowContainer.appendChild(gridContainer);
 }
